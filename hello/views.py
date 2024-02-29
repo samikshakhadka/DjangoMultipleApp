183138ed4376e7e5cfb53475b9d3e089af294857
Samiksha <swabhimankhadka2001@gmail.com>
2024-02-29 09:21:13 +0545

    feat(hello): render html page instead of static response

--- hello/views.py
+++ hello/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello everyone!!")
+    return render(request, "hello/index.html")
 
 def samiksha(request):
     return HttpResponse("Hello Samiksha")
 
 #parameterised palceholder to display dynamic greeting when the name is mentioned in the url
 def greet(request, name):
-    return HttpResponse(f"Hello,{name}!")
+    return HttpResponse(f"Hello,{name.capitalize()}!")
