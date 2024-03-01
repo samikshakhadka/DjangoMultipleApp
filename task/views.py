43171260f7616c32ca6be8bc241a29cb277d8e8f
Samiksha <swabhimankhadka2001@gmail.com>
2024-03-01 10:55:47 +0545

    feat(task): server side validation for POST request

--- task/views.py
+++ task/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value= 0 , max_value= 10)
 
 # Create your views here.
-tasks = ["cook", "clean", "sleep"]
+tasks = []
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks" : tasks
@@ -9,4 +18,18 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html" )
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task= form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse ("tasks:index"))
+        else:
+            return render(request, "tasks/index.html",
+                          {
+                              'form': form
+                          })
+    return render(request, "tasks/add.html" ,
+                  {
+                      "form" : NewTaskForm()
+                  })
