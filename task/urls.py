3965b1e0a1a059a7e67bce1e7e67400e6c240960
Samiksha <swabhimankhadka2001@gmail.com>
2024-03-01 10:15:59 +0545

    feat(task): template inheritance and namespace collision

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#creating app_name to prevent namespace collision while redirecting link from <a> tag
+app_name = "tasks"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add , name="add")
